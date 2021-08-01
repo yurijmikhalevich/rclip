22da113ab6bd7361391f64c51707bda02bd45e1c
Yurij Mikhalevich <yurij@mikhalevi.ch>
2021-08-01 20:59:02 +0300

    ci: build and publish an AppImage on `v*` tag (#3)

---
+++ appimage_after_bundle.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ "${GITHUB_ACTIONS}" ]]; then
+  # Install git into the appimage-builder docker image
+  apt-get update && apt-get install -y git
+fi
+
+PYTHONHOME=${APPDIR}/usr
+PYTHONPATH=${APPDIR}/usr/lib/python3.8/site-packages:${APPDIR}/usr/lib/python3.8
+LD_LIBRARY_PATH=${APPDIR}/usr/lib/x86_64-linux-gnu
+python3.8 -m pip install pipenv &&
+python3.8 -m pipenv lock -r > requirements.txt &&
+python3.8 -m pip install --upgrade --isolated --no-input --ignore-installed --prefix="${APPDIR}/usr" certifi wheel &&
+python3.8 -m pip install --upgrade --isolated --no-input --ignore-installed --prefix="${APPDIR}/usr" -r ./requirements.txt
