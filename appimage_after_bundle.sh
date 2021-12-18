7ebcf635bbf250b20e1969c6b4e9f20015123e6b
Yurij Mikhalevich <yurij@mikhalevi.ch>
2021-12-18 15:16:25 +0300

    removed support of the obsolete way to install pytorch on Linux

--- appimage_after_bundle.sh
+++ appimage_after_bundle.sh
@@ -7,10 +7,10 @@ if [[ "$GITHUB_ACTIONS" ]]; then
   apt-get update && apt-get install -y git
 fi
 
-PYTHONHOME=$APPDIR/usr
-PYTHONPATH=$APPDIR/usr/lib/python3.8/site-packages:$APPDIR/usr/lib/python3.8
-LD_LIBRARY_PATH=$APPDIR/usr/lib/x86_64-linux-gnu
-python3.8 -m pip install pipenv &&
+PYTHONHOME=$APPDIR/usr \
+PYTHONPATH=$APPDIR/usr/lib/python3.8/site-packages:$APPDIR/usr/lib/python3.8 \
+LD_LIBRARY_PATH=$APPDIR/usr/lib/x86_64-linux-gnu \
+python3.8 -m pip install pipenv==2021.11.23 &&
 python3.8 -m pipenv lock -r > requirements.txt &&
-python3.8 -m pip install --upgrade --isolated --no-input --ignore-installed --prefix="$APPDIR/usr" certifi wheel &&
-python3.8 -m pip install --upgrade --isolated --no-input --ignore-installed --prefix="$APPDIR/usr" -r ./requirements.txt -f https://download.pytorch.org/whl/torch_stable.html
+python3.8 -m pip install --upgrade --isolated --no-input --ignore-installed --prefix="$APPDIR/usr" certifi setuptools wheel &&
+python3.8 -m pip install --upgrade --isolated --no-input --ignore-installed --prefix="$APPDIR/usr" -r ./requirements.txt
