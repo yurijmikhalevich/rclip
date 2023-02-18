4713acae915710824bbb0ec8f6c7b03b1736641c
Yurij Mikhalevich <yurij@mikhalevi.ch>
2023-02-18 18:05:17 +0300

    fix build

--- appimage_after_bundle.sh
+++ appimage_after_bundle.sh
@@ -13,6 +13,6 @@ PYTHONPATH=$APPDIR/usr/lib/python3.8/site-packages:$APPDIR/usr/lib/python3.8 \
 LD_LIBRARY_PATH=$APPDIR/usr/lib/x86_64-linux-gnu \
 python3.8 -m pip install setuptools &&
 python3.8 -m pip install poetry==1.3.2 &&
-python3.8 -m poetry export --without dev -f requirements.txt --output requirements.txt &&
+python3.8 -m poetry export --without-hashes --without dev -f requirements.txt --output requirements.txt &&
 python3.8 -m pip install --upgrade --isolated --no-input --ignore-installed --prefix="$APPDIR/usr" certifi setuptools wheel &&
 python3.8 -m pip install --upgrade --isolated --no-input --ignore-installed --prefix="$APPDIR/usr" -r ./requirements.txt
