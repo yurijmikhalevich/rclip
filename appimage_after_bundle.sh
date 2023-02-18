056f6510e0479349a84c9d72b5f37fe5875d4700
Yurij Mikhalevich <yurij@mikhalevi.ch>
2023-02-18 11:44:18 +0300

    don't forget certifi

--- appimage_after_bundle.sh
+++ appimage_after_bundle.sh
@@ -10,7 +10,7 @@ fi
 PYTHONHOME=$APPDIR/usr \
 PYTHONPATH=$APPDIR/usr/lib/python3.8/site-packages:$APPDIR/usr/lib/python3.8 \
 LD_LIBRARY_PATH=$APPDIR/usr/lib/x86_64-linux-gnu \
-python3.8 -m pip install setuptools &&
+python3.8 -m pip install certifi setuptools &&
 python3.8 -m pip install poetry==1.3.2 &&
 python3.8 -m poetry export --without dev -f requirements.txt --output requirements.txt &&
 python3.8 -m pip install --upgrade --isolated --no-input --ignore-installed --prefix="$APPDIR/usr" certifi setuptools wheel &&
