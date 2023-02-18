b6f62bb9de342af74228eb98158a07f00a09eddb
Yurij Mikhalevich <yurij@mikhalevi.ch>
2023-02-18 18:57:56 +0300

    remove debug info and setuptools

--- appimage_after_bundle.sh
+++ appimage_after_bundle.sh
@@ -7,14 +7,9 @@ if [[ "$GITHUB_ACTIONS" ]]; then
   apt-get update && apt-get install -y git
 fi
 
-which appimage-builder || echo "no appimage-builder"
-
 PYTHONHOME=$APPDIR/usr \
 PYTHONPATH=$APPDIR/usr/lib/python3.8/site-packages:$APPDIR/usr/lib/python3.8 \
 LD_LIBRARY_PATH=$APPDIR/usr/lib/x86_64-linux-gnu \
-cat /etc/lsb-release &&
-which python3.8 &&
-python3.8 -m pip install setuptools &&
 python3.8 -m pip install poetry==1.3.2 &&
 python3.8 -m poetry export --without-hashes --without dev -f requirements.txt --output requirements.txt &&
 python3.8 -m pip install --upgrade --isolated --no-input --ignore-installed --prefix="$APPDIR/usr" certifi setuptools wheel &&
