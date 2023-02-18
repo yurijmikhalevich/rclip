d4d86d6cbc6b8f05a143b2bf0b289689d460d9fe
Yurij Mikhalevich <yurij@mikhalevi.ch>
2023-02-18 12:05:10 +0300

    fix libffi

--- appimage_after_bundle.sh
+++ appimage_after_bundle.sh
@@ -4,13 +4,13 @@ set -e
 
 if [[ "$GITHUB_ACTIONS" ]]; then
   # Install git into the appimage-builder docker image
-  apt-get update && apt-get install -y git
+  # And reinstall Python to ensure compatibility with libffi7
+  apt-get update && apt-get purge -y python3 && apt-get install -y git python3 python3-pip
 fi
 
 PYTHONHOME=$APPDIR/usr \
 PYTHONPATH=$APPDIR/usr/lib/python3.8/site-packages:$APPDIR/usr/lib/python3.8 \
 LD_LIBRARY_PATH=$APPDIR/usr/lib/x86_64-linux-gnu \
-python3.8 -m pip install certifi setuptools &&
 python3.8 -m pip install poetry==1.3.2 &&
 python3.8 -m poetry export --without dev -f requirements.txt --output requirements.txt &&
 python3.8 -m pip install --upgrade --isolated --no-input --ignore-installed --prefix="$APPDIR/usr" certifi setuptools wheel &&
