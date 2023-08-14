9fd1d8760f82ea704597edab397192872fa8aa4e
Yurij Mikhalevich <yurij@mikhalevi.ch>
2023-08-15 00:36:30 +0400

    nit

--- rclip/utils.py
+++ rclip/utils.py
@@ -61,7 +61,8 @@ def init_arg_parser() -> argparse.ArgumentParser:
     '  any query can be prefixed with a multiplier, e.g. "2:cat", "0.5:./cat-sleeps-on-a-chair.jpg";'
     ' adding a multiplier is especially useful when combining image and text queries because'
     ' image queries are usually weighted more than text ones\n\n'
-    'report a problem or suggest an improvement: https://github.com/yurijmikhalevich/rclip/issues',
+    'report a problem or suggest an improvement:\n'
+    '  https://github.com/yurijmikhalevich/rclip/issues\n\n',
   )
   version_str = f'rclip {version("rclip")}'
   parser.add_argument('--version', '-v', action='version', version=version_str, help=f'prints "{version_str}"')
