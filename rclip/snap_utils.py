311bff87c497ae9e3b2904174a7d166b6616cdfd
Yurij Mikhalevich <yurij@mikhalevi.ch>
2023-08-15 00:34:46 +0400

    fix

--- rclip/snap_utils.py
+++ rclip/snap_utils.py
@@ -7,34 +7,40 @@ def is_snap():
 
 
 def check_snap_permissions(directory: str):
-  is_any_file_in_a_dir = any(os.scandir(directory))
-  if is_any_file_in_a_dir:
-    # has access
-    return
-  homedir = os.path.expanduser('~')
-  if directory == homedir or directory.startswith(homedir + os.sep):
+  try:
+    any(os.scandir(directory))
+  except PermissionError:
+    homedir = os.getenv('SNAP_REAL_HOME')
+    if not homedir:
+      print(
+        'SNAP_REAL_HOME environment variable is not set.'
+        ' Please, report the issue to the rclip project on'
+        ' GitHub https://github.com/yurijmikhalevich/rclip/issues.'
+      )
+      sys.exit(1)
+    if directory == homedir or directory.startswith(homedir + os.sep):
+      print(
+        'rclip doesn\'t have access to the current directory.'
+        ' You can resolve this issue by running:'
+        '\n\n\tsudo snap connect rclip:home\n\n'
+        'This command will grant rclip the necessary access to the home directory.'
+        ' Afterward, you can try again.'
+      )
+      sys.exit(1)
+    if directory == '/media' or directory.startswith('/media' + os.sep):
+      print(
+        'rclip doesn\'t have access to the current directory.'
+        ' You can resolve this issue by running:'
+        '\n\n\tsudo snap connect rclip:removable-media\n\n'
+        'This command will grant rclip the necessary access to the "/media" directory.'
+        ' Afterward, you can try again.'
+      )
+      sys.exit(1)
     print(
-      'rclip is unable to locate any files in the current directory.'
-      ' If there are indeed files present, you can resolve this issue by running:'
-      '\n\n\tsudo snap connect rclip:home\n\n'
-      'This command will grant rclip the necessary access to the home directory.'
-      ' Afterward, you can try again.'
+      'Running rclip outside of the home or "/media" directories is not supported by its snap version.'
+      ' If you want to use rclip outside of home or "/media",'
+      ' file an issue in the rclip project on GitHub https://github.com/yurijmikhalevich/rclip/issues,'
+      ' describe your use case, and consider alternative rclip installation'
+      ' options https://github.com/yurijmikhalevich/rclip#linux.'
     )
     sys.exit(1)
-  if directory == '/media' or directory.startswith('/media' + os.sep):
-    print(
-      'rclip is unable to locate any files in the current directory.'
-      ' If there are indeed files present, you can resolve this issue by running:'
-      '\n\n\tsudo snap connect rclip:removable-media\n\n'
-      'This command will grant rclip the necessary access to the "/media" directory.'
-      ' Afterward, you can try again.'
-    )
-    sys.exit(1)
-  print(
-    'Running rclip outside of the home or "/media" directories is not supported by its snap version.'
-    ' If you want to use rclip outside of home or "/media",'
-    ' file an Issue in the rclip project on GitHub https://github.com/yurijmikhalevich/rclip/issues,'
-    ' describing your use case, and consider alternative rclip installation'
-    ' options https://github.com/yurijmikhalevich/rclip#linux.'
-  )
-  sys.exit(1)
