339151b1bb3cb51961c8e3da487551dee2ff1836
Yurij Mikhalevich <yurij@mikhalevi.ch>
2023-09-30 21:13:21 +0400

    wip

---
+++ benchmarks/similarity_search.py
@@ -0,0 +1,9 @@
+# generate vectors for the imagenet-1k dataset
+
+# take random 1k images for the first class
+# generate top-100 similarities for each of the images
+# compute percentage of similar images having the same class
+
+# repeate for every class
+
+# print average accuracy
