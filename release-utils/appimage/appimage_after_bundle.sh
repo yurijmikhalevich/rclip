ded6a5f1e9bf4fa876b6c43b199342bc9f231453
Yurij Mikhalevich <yurij@mikhalevi.ch>
2024-02-18 19:13:33 +0400

    fix(ci): appimage build (#97)

--- release-utils/appimage/appimage_after_bundle.sh
+++ release-utils/appimage/appimage_after_bundle.sh
@@ -5,7 +5,9 @@ set -e
 PYTHONHOME=$APPDIR/usr \
 PYTHONPATH=$APPDIR/usr/lib/python3.8/site-packages:$APPDIR/usr/lib/python3.8 \
 LD_LIBRARY_PATH=$APPDIR/usr/lib/x86_64-linux-gnu \
-python3.8 -m pip install poetry==1.5.1 &&
+python3.8 -m pip install poetry==1.7.1 &&
 python3.8 -m pip install --upgrade --isolated --no-input --ignore-installed --prefix="$APPDIR/usr" certifi setuptools wheel &&
 python3.8 -m poetry build &&
-python3.8 -m pip install --extra-index-url https://download.pytorch.org/whl/cpu --upgrade --isolated --no-input --ignore-installed --prefix="$APPDIR/usr" dist/*.whl
+python3.8 -m poetry export --output requirements.txt &&
+python3.8 -m pip install --extra-index-url https://download.pytorch.org/whl/cpu --upgrade --isolated --no-input --ignore-installed --prefix="$APPDIR/usr" -r requirements.txt &&
+python3.8 -m pip install --no-dependencies --isolated --no-input --prefix="$APPDIR/usr" dist/*.whl
