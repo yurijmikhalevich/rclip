f1301f2369704c35e92545e4cb010402db86dbd6
Yurij Mikhalevich <yurij@mikhalevi.ch>
2024-06-14 14:16:49 +0400

    linter

--- benchmarks/benchmark-imagenet.py
+++ benchmarks/benchmark-imagenet.py
@@ -4,7 +4,8 @@ import numpy as np
 from tqdm import tqdm
 from benchmarks.config import BATCH_SIZE, DATASET_DIR
 
-from benchmarks.datasets.imagenet_1k.classes import IMAGENET2012_CLASSES
+# 
+from benchmarks.datasets.imagenet_1k.classes import IMAGENET2012_CLASSES  # type: ignore
 
 from rclip import model, db
 from rclip.main import RClip
