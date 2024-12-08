299ea96af918676355b2d65dd2ccb3d6aa7f7f01
Yurij Mikhalevich <yurij@mikhalevi.ch>
2024-12-09 00:29:28 +0400

    install libc-bin

--- release-utils/appimage/appimage_after_bundle.sh
+++ release-utils/appimage/appimage_after_bundle.sh
@@ -3,7 +3,7 @@
 set -e
 
 PYTHONHOME=$APPDIR/usr \
-PYTHONPATH=$APPDIR/usr/lib/python3.10/site-packages:$APPDIR/usr/lib/python3.10 \
+PYTHONPATH=$APPDIR/usr/lib/python3/dist-packages:$APPDIR/usr/lib/python3.10 \
 LD_LIBRARY_PATH=$APPDIR/usr/lib/x86_64-linux-gnu \
 python3.10 -m pip install poetry==1.8.4 &&
 python3.10 -m pip install --upgrade --isolated --no-input --ignore-installed --prefix="$APPDIR/usr" certifi setuptools wheel &&
