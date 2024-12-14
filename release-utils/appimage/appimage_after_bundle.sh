95cc6a58d20089fd20cefa3dc8586709ccfc9318
Yurij Mikhalevich <yurij@mikhalevi.ch>
2024-12-14 21:10:21 +0400

    fix: AppImage build (#154)

--- release-utils/appimage/appimage_after_bundle.sh
+++ release-utils/appimage/appimage_after_bundle.sh
@@ -3,9 +3,9 @@
 set -e
 
 PYTHONHOME=$APPDIR/usr \
-PYTHONPATH=$APPDIR/usr/lib/python3.10/site-packages:$APPDIR/usr/lib/python3.10 \
+PYTHONPATH=$APPDIR/usr/lib/python3/dist-packages:$APPDIR/usr/lib/python3.10 \
 LD_LIBRARY_PATH=$APPDIR/usr/lib/x86_64-linux-gnu \
-curl -sS https://bootstrap.pypa.io/get-pip.py | python3.10 &&
+which python3.10 &&
 python3.10 -m pip install poetry==1.8.4 &&
 python3.10 -m pip install --upgrade --isolated --no-input --ignore-installed --prefix="$APPDIR/usr" certifi setuptools wheel &&
 python3.10 -m poetry build &&
