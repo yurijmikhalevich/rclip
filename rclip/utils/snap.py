768348754006380e87bc57d92ff7bebfbb98506f
Yurij Mikhalevich <yurij@mikhalevi.ch>
2025-08-18 21:34:38 +1000

    extract the directory name conditional from the f-string with the error message

--- rclip/utils/snap.py
+++ rclip/utils/snap.py
@@ -14,9 +14,11 @@ def get_snap_permission_error(directory: str, is_current_directory: bool = False
       " Please, report the issue to the rclip project on"
       " GitHub https://github.com/yurijmikhalevich/rclip/issues."
     )
+
+  directory_str = "the current directory" if is_current_directory else directory
   if directory == homedir or directory.startswith(homedir + os.sep):
     return (
-      f"rclip doesn't have access to {'the current directory' if is_current_directory else directory}."
+      f"rclip doesn't have access to {directory_str}."
       " You can resolve this issue by running:"
       "\n\n\tsudo snap connect rclip:home\n\n"
       "This command will grant rclip the necessary access to the home directory."
@@ -24,7 +26,7 @@ def get_snap_permission_error(directory: str, is_current_directory: bool = False
     )
   if directory == "/media" or directory.startswith("/media" + os.sep):
     return (
-      f"rclip doesn't have access to {'the current directory' if is_current_directory else directory}."
+      f"rclip doesn't have access to {directory_str}."
       " You can resolve this issue by running:"
       "\n\n\tsudo snap connect rclip:removable-media\n\n"
       'This command will grant rclip the necessary access to the "/media" directory.'
