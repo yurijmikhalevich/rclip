69a591db9ebe72cb913eeae26cac050913496304
Yurij Mikhalevich <yurij@mikhalevi.ch>
2026-05-10 13:56:34 +1000

    chore: migrate from `poetry` to `uv` (#259)
    ## How does this PR impact the user?
    
    No impact, but devs will get faster local dev experience and faster CI.
    
    ## Description
    
    Ditto.
    
    ## Limitations
    
    N/A
    
    ## Checklist
    
    - [x] my PR is focused and contains one holistic change
    - [ ] I have added screenshots or screen recordings to show the changes

--- release-utils/appimage/appimage_after_bundle.sh
+++ release-utils/appimage/appimage_after_bundle.sh
@@ -6,10 +6,10 @@ PYTHONHOME=$APPDIR/usr \
 PYTHONPATH=$APPDIR/usr/lib/python3/dist-packages:$APPDIR/usr/lib/python3.11 \
 LD_LIBRARY_PATH=$APPDIR/usr/lib/x86_64-linux-gnu \
 which python3.11 &&
-python3.11 -m pip install poetry==1.8.4 &&
+python3.11 -m pip install uv==0.11.12 &&
 python3.11 -m pip install --upgrade --isolated --no-input --ignore-installed --prefix="$APPDIR/usr" setuptools wheel &&
-python3.11 -m poetry build &&
-python3.11 -m poetry export --output requirements.txt &&
+python3.11 -m uv build &&
+python3.11 -m uv export --locked --format requirements.txt --no-dev --no-editable --no-emit-project --no-hashes --output-file requirements.txt &&
 python3.11 -m pip install --upgrade --isolated --no-input --ignore-installed --prefix="$APPDIR/usr" -r requirements.txt &&
 python3.11 -m pip install --no-dependencies --isolated --no-input --prefix="$APPDIR/usr" dist/*.whl &&
 python3.11 -m pip install --upgrade --isolated --no-input --ignore-installed --target="$APPDIR/usr/lib/python3.11/site-packages" certifi
